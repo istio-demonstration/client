1e0c8824a3cda312a51afa8912f63cc98497d27d
Parker <jwk839928622@outlook.com>
2021-03-16 02:01:42 +0800

    add full api address

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  baseUrl: 'api.chatroom365.com/api/',
-  hubUrl: 'api.chatroom365.com/hubs/'
+  baseUrl: 'http://api.chatroom365.com/api/',
+  hubUrl: 'http://api.chatroom365.com/hubs/'
 };
